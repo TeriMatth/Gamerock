911c52889f4bda16797fee1b62056dbd61473a9a
matthew <tersughm@gmail.com>
2023-08-24 13:16:52 +0100

    Variable for the buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,8 @@
 //Creating variables for the player, computer and result
 const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText")
+const resultText = document.querySelector("#resultText");
+
+// Variable for the buttons
+
+const choiceBtns = document.querySelectorAll(".choiceBtn");
