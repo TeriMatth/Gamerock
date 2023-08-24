5e4c7935334ccf7f5f9691a88c761f9c2a8eb98d
matthew <tersughm@gmail.com>
2023-08-24 14:05:41 +0100

    Creating choice variables and iterating over the buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,3 +6,22 @@ const resultText = document.querySelector("#resultText");
 // Variable for the buttons
 
 const choiceBtns = document.querySelectorAll(".choiceBtn");
+
+
+//Create variables for player, computer choice and result
+
+let playerChoice;
+let computerChoice;
+let result;
+
+//Iterating over the buttons to make a choice
+choiceBtns.forEach(button => button.addEventlistener("click", () => {
+    playerChoice = button.textContent;
+    //Invoke a function for computer choice
+    computerTurn()
+    playerText.textContent = `Player: ${playerChoice}`;
+    computerText.textContent = `Computer: ${computerChoice}`;
+    //Invoke a winner function for the result
+    resultText = checkWinner();
+
+}))
