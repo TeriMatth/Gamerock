a31c83517c35e3e74a4ddfbc4ba5249c1b9c0640
matthew <tersughm@gmail.com>
2023-08-24 13:14:21 +0100

    Variable for player, computer and result.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,4 @@
+//Creating variables for the player, computer and result
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText")
